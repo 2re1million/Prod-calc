4ee1cb37372b684a869906294a8b2c73f913b98a
2re1million <56553678+2re1million@users.noreply.github.com>
2023-09-26 11:42:02 +0200

    Update app.py
    test av bakgrunn

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
+import base64
+
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = """
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    """ % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/back.png')
+
+
+
 def calculate_savings(minutes_saved, num_employees, num_years, avg_salary):
     # Konverterer minutter til timer
     hours_saved_per_day = minutes_saved / 60
