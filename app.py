79d67d962045d93892dc2a342cfc5a2de800d30a
2re1million <56553678+2re1million@users.noreply.github.com>
2023-09-27 20:21:46 +0200

    Update app.py
    Uke ikke dag
    Sosiale kostnader med

--- app.py
+++ app.py
@@ -25,18 +25,17 @@ def totale_kostnader_for_bedriften(arslonn: float) -> float:
 
     return total_kostnad
 
-def calculate_savings(minutes_saved, num_employees, num_years, avg_salary):
-    # Konverterer minutter til timer
-    hours_saved_per_day = minutes_saved / 60
-    # Antall arbeidsdager i et år (vanligvis ca. 250)
-    work_days_per_year = 230
+def calculate_savings(hours_saved_per_week, num_employees, num_years, avg_salary):
+    # Antall arbeidsuker i et år (vanligvis ca. 50)
+    work_weeks_per_year = 50
     # Total antall timer spart per år
-    total_hours_saved = hours_saved_per_day * work_days_per_year * num_employees
+    total_hours_saved = hours_saved_per_week * work_weeks_per_year * num_employees
     # Konverter lønn fra årlig til timebasis
-    avg_hourly_salary = avg_salary / (work_days_per_year * 7.5)
+    avg_hourly_salary = avg_salary / (work_weeks_per_year * 7.5 * 5)  # assuming 7.5 hours/day and 5 days/week
     # Total besparelse
     total_savings = total_hours_saved * avg_hourly_salary * num_years
     return total_savings
+
 
 st.markdown('## Din innsparingskalkulator: ')
 st.markdown('### Hva koster manuelt arbeid bedriften virkelig?')
@@ -49,8 +48,8 @@ Heldigvis kan dette gjøres noe med! Med riktig teknologi kan manuelt arbeid dig
 st.write("Bruk kalkulatoren under og avslør hvor mye du faktisk kan spare ved å digitalisere disse gjentagende arbeidsoppgavene")
 
 # Inputs fra bruker
-X = st.slider('Hvor mange minutter bruker en ansatt på repetitive manuelle oppgaver hver dag?', 0, 120, 30, step=5)
-Y = int(st.text_input('Hvor mange ansatte utfører disse oppgavene regelmessig?', 5))
+X = st.slider('Timer per uke en ansatt bruker på repetitiv oppgaveer?)', 0, 25, 4, step=0.5)
+Y = st.number_input('Antall ansatte som jevnlig utfører disse oppgaven hver uke?', 5))
 W = st.number_input('Gjennomsnittlig årlig lønn for disse ansatte (i NOK):', value=664680.0, format='%f')
 Z = st.slider('Hvor mange år ønsker du å se potensielle besparelser for?', 1, 10, 2)
 
@@ -70,7 +69,9 @@ if savings > 0:
     st.write(f"Tenk deg å kunne frigjøre opptil:")
     st.write(f" NOK {savings:,.2f} over {Z} år! 💸")
     st.write(f"Dette er verdifulle ressurser som kan reinvesteres i andre produktive områder av virksomheten din.")
-    st.write(" **La WS vise vei!** 🚀")
+    #st.write(" **La WS vise vei!** 🚀")
     st.write("Vi i WS er spesialister på å digitalisere manuelle oppgaver. Gi dine ansatte gaven av tid, og la oss hjelpe dere med å maksimere effektiviteten. Kontakt oss, og vi tar steget sammen - post@webstep.no")
 else:
     st.write("Selv mindre effektivitetsøkninger kan akkumuleres til betydelige besparelser over tid. La WS være din guide på veien mot digitalisering.")
+
+    st.write("* - Regnestykket inkl arb.avgift og andre sosiale kostnaderfor arb.giver.)
