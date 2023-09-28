022093f9e7ae1a53e33d066528287aba5fa81083
2re1million <56553678+2re1million@users.noreply.github.com>
2023-09-28 21:06:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 def totale_kostnader_for_bedriften(arslonn: float) -> float:
     # Feriepenger
@@ -62,15 +63,20 @@ with col1:
 with col2:
 
 
-    # Resultat:
-    st.write(f"🔍 **Ditt potensiale:**")
-
-    savings_int = int(savings)
-
+    # A/B testing
+    variant = random.choice(['A', 'B'])
     if savings > 0:
         st.write(f" Besparelse: NOK **{savings_int:,}** over **{Z}** år!💸")
         st.write(f"Dette er verdifulle ressurser som kan reinvesteres i andre produktive områder av virksomheten din. Og mer fornøyde og motiverte ansatte er en heldig bi-effekt som kommer på toppen.")
         
-        st.write("Hjelp til å komme i gang? 👉 post@webstep.no")
+        # Hvis variant A
+        if variant == 'A':
+            st.write("Trenger du hjelp? Kontakt oss!")
+            st.button("Send oss en e-post")
+            
+        # Hvis variant B
+        elif variant == 'B':
+            link = '[Trenger du hjelp? Klikk her for mer informasjon!](https://dinlandingside.no)'
+            st.markdown(link, unsafe_allow_html=True)
     else:
         st.write("Selv mindre effektivitetsøkninger kan akkumuleres til betydelige besparelser over tid. Prosjekter kan med det betale seg ned av seg selv over tid")
